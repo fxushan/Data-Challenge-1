408e871f9d8116f7d7f82e62655dbb9ee2ff71a0
KyV3n <k.l.x.venneman@student.tue.nl>
2024-03-11 23:01:50 +0100

    Add Confusion Matrix, Cohen's Kappa, MCC

--- dc1/main.py
+++ dc1/main.py
@@ -1,28 +1,28 @@
 # Custom imports
+import argparse
+import os
+from datetime import datetime
+from pathlib import Path
+from typing import List
+
+# Other imports
+import matplotlib.pyplot as plt  # type: ignore
+import plotext  # type: ignore
+import seaborn as sns
+# Torch imports
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from matplotlib.pyplot import figure
+from torchsummary import summary  # type: ignore
+
 from dc1.batch_sampler import BatchSampler
 from dc1.image_dataset import ImageDataset
 from dc1.net import Net
 from dc1.train_test import train_model, test_model
 
-# Torch imports
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torchsummary import summary  # type: ignore
-
-# Other imports
-import matplotlib.pyplot as plt  # type: ignore
-from matplotlib.pyplot import figure
-import os
-import argparse
-import plotext  # type: ignore
-from datetime import datetime
-from pathlib import Path
-from typing import List
-
 
 def main(args: argparse.Namespace, activeloop: bool = True) -> None:
-
     # Load the train and test data set
     train_dataset = ImageDataset(Path("data/X_train.npy"), Path("data/Y_train.npy"))
     test_dataset = ImageDataset(Path("data/X_test.npy"), Path("data/Y_test.npy"))
@@ -42,18 +42,19 @@ def main(args: argparse.Namespace, activeloop: bool = True) -> None:
     # the structure of your model (GPU acceleration hides them)!
     # Also make sure you set this to False again for actual model training
     # as training your model with GPU-acceleration (CUDA/MPS) is much faster.
-    DEBUG = True
+    DEBUG = False
 
     # Moving our model to the right device (CUDA will speed training up significantly!)
     if torch.cuda.is_available() and not DEBUG:
         print("@@@ CUDA device found, enabling CUDA training...")
         device = "cuda"
+        # device = physical_devices[0]
         model.to(device)
         # Creating a summary of our model and its layers:
         summary(model, (1, 128, 128), device=device)
     elif (
-        torch.backends.mps.is_available() and not DEBUG
-    ):  # PyTorch supports Apple Silicon GPU's from version 1.12
+            torch.backends.mps.is_available() and not DEBUG
+    ):  # PyTorch supports Apple Silicon GPUs from version 1.12
         print("@@@ Apple silicon device enabled, training with Metal backend...")
         device = "mps"
         model.to(device)
@@ -63,7 +64,7 @@ def main(args: argparse.Namespace, activeloop: bool = True) -> None:
         # Creating a summary of our model and its layers:
         summary(model, (1, 128, 128), device=device)
 
-    # Lets now train and test our model for multiple epochs:
+    # Let us now train and test our model for multiple epochs:
     train_sampler = BatchSampler(
         batch_size=batch_size, dataset=train_dataset, balanced=args.balanced_batches
     )
@@ -73,26 +74,71 @@ def main(args: argparse.Namespace, activeloop: bool = True) -> None:
 
     mean_losses_train: List[torch.Tensor] = []
     mean_losses_test: List[torch.Tensor] = []
-    
+    mean_kappas_train = []
+    mean_kappas_test = []
+    mean_mcc_train = []
+    mean_mcc_test = []
+
     for e in range(n_epochs):
         if activeloop:
-
             # Training:
-            losses = train_model(model, train_sampler, optimizer, loss_function, device)
+            losses, kappas, mcc_list, conf_matrix_total_train = train_model(model, train_sampler, optimizer,
+                                                                            loss_function, device)
+
             # Calculating and printing statistics:
+            # Cross entropy loss
             mean_loss = sum(losses) / len(losses)
             mean_losses_train.append(mean_loss)
             print(f"\nEpoch {e + 1} training done, loss on train set: {mean_loss}\n")
 
-            # Testing:
-            losses = test_model(model, test_sampler, loss_function, device)
+            # Confusion matrix
+            print(conf_matrix_total_train)
 
-            # # Calculating and printing statistics:
+            # Cohen's Kappa
+            # Average kappa over all batches
+            mean_kappa = sum(kappas) / len(kappas)
+            mean_kappas_train.append(mean_kappa)
+            print(f"\nEpoch {e + 1} training done, average Cohen's kappa train set: {mean_kappa}\n")
+
+            # Matthew's correlation coefficient
+            # Average MCC over all batches
+            mean_mcc = sum(mcc_list) / len(mcc_list)
+            mean_mcc_train.append(mean_mcc)
+            print(f"\nEpoch {e + 1} training done, average MCC train set: {mean_mcc}\n")
+
+            #     # Cohen's kappa over final confusion matrix
+            # Po = sum(conf_matrix_total[i][i] for i in range(6)) / np.sum(conf_matrix_total)
+            # row_sums = [sum(row) for row in conf_matrix_total]
+            # col_sums = [sum(col) for col in zip(*conf_matrix_total)]
+            # Pe = sum((row_sums[i] * col_sums[i]) for i in range(6)) / (np.sum(conf_matrix_total) ** 2)
+            # kappa_conf_matrix = (Po - Pe) / (1 - Pe)
+            # print(f"\nEpoch {e + 1} training done, kappa over confusion matrix in train set: {kappa_conf_matrix}\n")
+
+            # Testing:
+            losses, kappas, mcc_list, conf_matrix_total_test = test_model(model, test_sampler, loss_function, device)
+
+            # Calculating and printing statistics:
+            # Cross entropy loss
             mean_loss = sum(losses) / len(losses)
             mean_losses_test.append(mean_loss)
             print(f"\nEpoch {e + 1} testing done, loss on test set: {mean_loss}\n")
 
-            ### Plotting during training
+            # Confusion matrix
+            print(conf_matrix_total_test)
+
+            # Cohen's Kappa
+            # Average kappa over all batches
+            mean_kappa = sum(kappas) / len(kappas)
+            mean_kappas_test.append(mean_kappa)
+            print(f"\nEpoch {e + 1} training done, average Cohen's kappa test set: {mean_kappa}\n")
+
+            # Matthew's correlation coefficient
+            # Average MCC over all batches
+            mean_mcc = sum(mcc_list) / len(mcc_list)
+            mean_mcc_test.append(mean_mcc)
+            print(f"\nEpoch {e + 1} training done, average MCC test set: {mean_mcc}\n")
+
+            # Plotting during training
             plotext.clf()
             plotext.scatter(mean_losses_train, label="train")
             plotext.scatter(mean_losses_test, label="test")
@@ -107,24 +153,63 @@ def main(args: argparse.Namespace, activeloop: bool = True) -> None:
     # check if model_weights/ subdir exists
     if not Path("model_weights/").exists():
         os.mkdir(Path("model_weights/"))
-    
+
     # Saving the model
     torch.save(model.state_dict(), f"model_weights/model_{now.month:02}_{now.day:02}_{now.hour}_{now.minute:02}.txt")
-    
+
     # Create plot of losses
-    figure(figsize=(9, 10), dpi=80)
+    figure(figsize=(9, 16), dpi=80)
     fig, (ax1, ax2) = plt.subplots(2, sharex=True)
-    
+
     ax1.plot(range(1, 1 + n_epochs), [x.detach().cpu() for x in mean_losses_train], label="Train", color="blue")
     ax2.plot(range(1, 1 + n_epochs), [x.detach().cpu() for x in mean_losses_test], label="Test", color="red")
     fig.legend()
-    
+    fig.suptitle('Cross Entropy loss over epochs')
+
     # Check if /artifacts/ subdir exists
     if not Path("artifacts/").exists():
         os.mkdir(Path("artifacts/"))
 
     # save plot of losses
-    fig.savefig(Path("artifacts") / f"session_{now.month:02}_{now.day:02}_{now.hour}_{now.minute:02}.png")
+    fig.savefig(Path("artifacts") / f"CrossEntropy_session_{now.month:02}_{now.day:02}_{now.hour}_{now.minute:02}.png")
+
+    # Create plot of heatmaps of final confusion matrix
+    fig, axs = plt.subplots(2, figsize=(10, 10))
+
+    sns.heatmap(conf_matrix_total_train, annot=True, fmt='d', ax=axs[0])
+    axs[0].set_ylabel('True label')
+    axs[0].set_xlabel('Predicted label')
+    axs[0].set_title('Final heatmap for train data')
+
+    sns.heatmap(conf_matrix_total_test, annot=True, fmt='d', ax=axs[1])
+    axs[1].set_ylabel('True label')
+    axs[1].set_xlabel('Predicted label')
+    axs[1].set_title('Final heatmap for test data')
+
+    fig.savefig(
+        Path("artifacts") / f"ConfusionMatrix_session_{now.month:02}_{now.day:02}_{now.hour}_{now.minute:02}.png")
+
+    # Create plot of Cohen's Kappas
+    figure(figsize=(9, 10), dpi=80)
+    fig, (ax1, ax2) = plt.subplots(2, sharex=True)
+
+    ax1.plot(range(1, 1 + n_epochs), [x for x in mean_kappas_train], label="Train", color="blue")
+    ax2.plot(range(1, 1 + n_epochs), [x for x in mean_kappas_test], label="Test", color="red")
+    fig.legend()
+    fig.suptitle("Cohen's Kappa over epochs")
+
+    fig.savefig(Path("artifacts") / f"CohenKappa_session_{now.month:02}_{now.day:02}_{now.hour}_{now.minute:02}.png")
+
+    # Create plot of Matthews correlation coefficients
+    figure(figsize=(9, 10), dpi=80)
+    fig, (ax1, ax2) = plt.subplots(2, sharex=True)
+
+    ax1.plot(range(1, 1 + n_epochs), [x for x in mean_mcc_train], label="Train", color="blue")
+    ax2.plot(range(1, 1 + n_epochs), [x for x in mean_mcc_test], label="Test", color="red")
+    fig.legend()
+    fig.suptitle('Matthews Correlation Coefficient over epochs')
+
+    fig.savefig(Path("artifacts") / f"MCC_session_{now.month:02}_{now.day:02}_{now.hour}_{now.minute:02}.png")
 
 
 if __name__ == "__main__":
--- dc1/train_test.py
+++ dc1/train_test.py
@@ -1,8 +1,12 @@
-from tqdm import tqdm
+from typing import Callable, List, Any
+
 import torch
-from dc1.net import Net
+from sklearn.metrics import cohen_kappa_score, confusion_matrix, matthews_corrcoef
+from tensorflow import Tensor
+from tqdm import tqdm
+
 from dc1.batch_sampler import BatchSampler
-from typing import Callable, List
+from dc1.net import Net
 
 
 def train_model(
@@ -11,9 +15,13 @@ def train_model(
         optimizer: torch.optim.Optimizer,
         loss_function: Callable[..., torch.Tensor],
         device: str,
-) -> List[torch.Tensor]:
-    # Lets keep track of all the losses:
+) -> tuple[List[Tensor], List[float], List[float], int | Any]:
+    #
+    # Let us keep track of all the losses:
     losses = []
+    kappas = []
+    mcc_list = []
+    conf_matrix_total = 0
     # Put the model in train mode:
     model.train()
     # Feed all the batches one by one:
@@ -24,17 +32,55 @@ def train_model(
         x, y = x.to(device), y.to(device)
         # Get predictions:
         predictions = model.forward(x)
+
+        # Get probabilities and predicted classes
+        probabilities = torch.nn.functional.softmax(predictions, dim=1)
+        _, predicted_classes = torch.max(probabilities, dim=1)
+
+        # Accuracy metric
+        accuracy = (predicted_classes == y).float().mean()
+
+        # Cross entropy
         loss = loss_function(predictions, y)
         losses.append(loss)
+
+        # Cohen's kappa
+        predictions_np = predicted_classes.detach().cpu().numpy()
+        labels_np = y.detach().cpu().numpy()
+        kappa = cohen_kappa_score(predictions_np, labels_np)
+        kappas.append(kappa)
+
+        # Matthew's Correlation Coefficient
+        mcc = matthews_corrcoef(predictions_np, labels_np)
+        mcc_list.append(mcc)
+
+        # print(predictions)
+        # print(probabilities)
+        # print('\n')
+        # print(predicted_classes)
+        # print(y)
+        #
+        # print(f'Accuracy: {accuracy}')
+        # print(f'Cross entropy: {loss}')
+        # print(f"Cohen's Kappa: {kappa}")
+        # print(f"MCC: {mcc}")
+
+        # Confusion Matrix
+        labels = list(range(0, 6))
+        conf_matrix_batch = confusion_matrix(predictions_np, labels_np, labels=labels)
+        # print(conf_matrix_batch)
+
+        conf_matrix_total = conf_matrix_batch + conf_matrix_total
+
         # We first need to make sure we reset our optimizer at the start.
-        # We want to learn from each batch seperately,
+        # We want to learn from each batch separately,
         # not from the entire dataset at once.
         optimizer.zero_grad()
-        # We now backpropagate our loss through our model:
+        # We now back-propagate our loss through our model:
         loss.backward()
         # We then make the optimizer take a step in the right direction.
         optimizer.step()
-    return losses
+    return losses, kappas, mcc_list, conf_matrix_total
 
 
 def test_model(
@@ -42,10 +88,13 @@ def test_model(
         test_sampler: BatchSampler,
         loss_function: Callable[..., torch.Tensor],
         device: str,
-) -> List[torch.Tensor]:
+) -> tuple[List[Tensor], List[float], List[float], int | Any]:
     # Setting the model to evaluation mode:
     model.eval()
     losses = []
+    kappas = []
+    mcc_list = []
+    conf_matrix_total = 0
     # We need to make sure we do not update our model based on the test data:
     with torch.no_grad():
         for (x, y) in tqdm(test_sampler):
@@ -53,6 +102,27 @@ def test_model(
             x = x.to(device)
             y = y.to(device)
             prediction = model.forward(x)
+
+            # Get probabilities and predicted classes
+            probabilities = torch.nn.functional.softmax(prediction, dim=1)
+            _, predicted_classes = torch.max(probabilities, dim=1)
+
+            # Cross entropy
             loss = loss_function(prediction, y)
             losses.append(loss)
-    return losses
+
+            # Cohen's Kappa
+            predictions_np = predicted_classes.detach().cpu().numpy()
+            labels_np = y.detach().cpu().numpy()
+            kappa = cohen_kappa_score(predictions_np, labels_np)
+            kappas.append(kappa)
+
+            # Matthew's Correlation Coefficient
+            mcc = matthews_corrcoef(predictions_np, labels_np)
+            mcc_list.append(mcc)
+
+            # Confusion matrix
+            labels = list(range(0, 6))
+            conf_matrix_batch = confusion_matrix(predictions_np, labels_np, labels=labels)
+            conf_matrix_total = conf_matrix_batch + conf_matrix_total
+    return losses, kappas, mcc_list, conf_matrix_total
