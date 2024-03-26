716e17ba24d1d1b125ba1d1325e4856a824206cb
Ari <a.craciun@student.tue.nl>
2024-03-27 00:56:49 +0100

    fix net, previously added density figure

--- dc1/net.py
+++ dc1/net.py
@@ -1,4 +1,3 @@
-
 import torch
 import torch.nn as nn
 
@@ -20,8 +19,6 @@ class StochasticDepth(nn.Module):
 
 
 class Net(nn.Module):
-    # Leaky ReLU slope_1: float = 0.6807524246386062, slope_2: float = 0.053947204697184746, slope_3: float = 0.5569877536243514
-    # PReLU alpha_1=0.7944129993352669, alpha_2=0.340813568624628, alpha_3=0.37811389136107976
     def __init__(self, n_classes: int = 6, alpha_1=1.0887779628058707, alpha_2=1.6631499652552382, alpha_3=1.9301193111245831) -> None:
         super(Net, self).__init__()
         self.cnn_layers = nn.Sequential(
@@ -44,17 +41,23 @@ class Net(nn.Module):
             nn.Dropout(p=0.125),
         )
 
-        # Adjusting the size after convolutional layers
-        self.flatten_size = 256 * 6 * 6  # Update based on the output size of the last conv layer
+        # Calculate the size of the output from convolutional layers
+        self.flatten_size = self._get_flatten_size()
 
         self.linear_layers = nn.Sequential(
-            nn.Linear(self.flatten_size, 512),
+            nn.Linear(self.flatten_size, 512),  # Adjusted input size
             nn.ReLU(inplace=True),
             nn.Linear(512, n_classes)
         )
 
+    def _get_flatten_size(self):
+        # Define a dummy input tensor to calculate the output size of the convolutional layers
+        input_tensor = torch.randn(1, 1, 128, 128)
+        with torch.no_grad():
+            features = self.cnn_layers(input_tensor)
+        # Calculate the total number of features after flattening
+        return features.view(1, -1).size(1)
 
-    # Defining the forward pass
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         if x.shape[1] == 1:
             x = x
@@ -62,9 +65,6 @@ class Net(nn.Module):
             x = x.permute(0, 3, 1, 2)
 
         x = self.cnn_layers(x)
-        # After our convolutional layers which are 2D, we need to flatten our
-        # input to be 1 dimensional, as the linear layers require this.
         x = x.reshape(x.size(0), -1)
-
         x = self.linear_layers(x)
         return x
